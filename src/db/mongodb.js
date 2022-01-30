73094c63fcd92554ab69e57b146b86716d45043a
Awais Kamran <awaiskamran@ITs-MacBook-Pro-4.local>
2022-01-30 23:06:58 +0500

    study-mapper-node: feat(Application Structure) initial application structure added.

---
+++ src/db/mongodb.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { logger } = require("../utils");
+
+mongoose.Promise = global.Promise;
+
+// const MONGODB_URI = `mongodb://${username}:${password}@${host}:${port}/${databaseName}`;
+
+const options = {
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  useNewUrlParser: true,
+};
+
+// if (!process.env.MONGODB_URI) {
+//   logger.error('Please set MONGO_URI');
+//   process.exit(-1);
+// }
+
+// mongoose.connect(process.env.MONGODB_URI, options);
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   auth: {
+//     user: username,
+//     password: password
+//   },
+//   options,
+// });
+
+mongoose.connection.on("connected", () => {
+  logger.info("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  logger.error("MongoDB connection error:", err);
+  process.exit(-1);
+});
+
+mongoose.connection.on("disconnected", () => {
+  logger.error("MongoDB disconnected");
+});
+
+module.exports = mongoose;
