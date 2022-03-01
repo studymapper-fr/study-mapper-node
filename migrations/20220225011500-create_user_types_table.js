84c41fad293f72885d89683fbf6021e1c44e31a1
mahamz <maham.zafar@studymapper.com>
2022-03-01 14:38:55 +0000

    fix: removed seeder and used migration to insert data in userTypes table

--- migrations/20220225011500-create_user_types_table.js
+++ migrations/20220225011500-create_user_types_table.js
@@ -3,7 +3,15 @@ const UserTypesSchema = require("../src/schema/UserTypes");
 const TABLE_NAME = "userTypes";
 module.exports = {
   async up (queryInterface) {
-     return queryInterface.createTable(TABLE_NAME, UserTypesSchema);
+     return queryInterface.createTable(TABLE_NAME, UserTypesSchema).then(() => {
+       queryInterface.bulkInsert('userTypes', [{
+        type: "STUDENT"
+      }, {
+        type: "SCHOOL"
+      },{
+        type: "COMPANY"
+      }]);
+     });
   },
 
   async down (queryInterface) {
--- migrations/20220226194503-add_user_type_column.js
+++ migrations/20220226194503-add_user_type_column.js
@@ -5,18 +5,18 @@ module.exports = {
      await queryInterface.addColumn(TABLE_NAME, 'userTypeId', {
        type: Sequelize.DataTypes.INTEGER(11),
        defaultValue: 1
-     });
-     queryInterface.addConstraint(TABLE_NAME, {
+     }).then(() => queryInterface.addConstraint(TABLE_NAME, {
       fields: ['userTypeId'],
       type: 'foreign key',
       name: 'custom_fkey_user_type_id',
-      references: { //Required field
+      references: { 
         table: 'userTypes',
         field: 'id'
       },
       onDelete: 'no action',
       onUpdate: 'no action'
-    });
+    }));
+
   },
 
   async down (queryInterface) {
