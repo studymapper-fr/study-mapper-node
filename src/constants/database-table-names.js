c971970c4bfb73800108dd47beb20ef11c043f0e
Awais Kamran <96591044+awaiskamranchaudhary@users.noreply.github.com>
2022-03-14 13:26:47 +0500

    CU-220kezr - Create Schema For Educational Details (BE) (#3)
    Co-authored-by: Awais Kamran <awaiskamran@ITs-MacBook-Pro-5.local>

--- src/constants/database-table-names.js
+++ src/constants/database-table-names.js
@@ -1,7 +1,9 @@
 const USER = "user";
 const WORK_DETAIL = "user-work-detail";
+const EDUCATION_DETAIL = "user-education-detail";
 
 module.exports = {
   USER,
   WORK_DETAIL,
+  EDUCATION_DETAIL,
 };
